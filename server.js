da547690f941e1f6e1b892dfdae3407e5f40b738
PrimeOsita <brightstarositadinma@gmail.com>
2026-08-20 04:53:47 +0100

    fix: correct case of public directory in server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 // Request logger
 app.use((req, res, next) => {
